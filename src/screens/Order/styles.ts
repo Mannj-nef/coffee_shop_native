1024f6b16f5bfca344da24e32388017a08472b94
Manh Quan <manhquan.05012002@gmail.com>
2023-11-30 17:04:53 +0700

    update code

--- src/screens/Order/styles.ts
+++ src/screens/Order/styles.ts
@@ -1,7 +1,14 @@
 import { StyleSheet } from 'react-native'
+import { COLORS } from '../../themes'
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.primaryDarkGreyHex,
+  },
+  propertyText: {
+    color: COLORS.primaryWhiteHex,
+  },
 })
 
 export default styles
